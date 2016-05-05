83d18b6d0bb9d4f59e93a4c9cb28e9daaed14518
Brian Walen <bwalen@hotmail.com>
2016-05-05 14:34:06 -0700

    I wrote a basic back end to serve the game.

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser= require("body-parser");
+var app = express();
+var request = require("request");
+var mongo = require('mongodb');
+var myClient = mongo.MongoClient;
+var url = "mongodb://bwalen:alookback@ds013162.mlab.com:13162/bwalen";
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+
+app.listen(port, function(){
+  console.log("Listening on port " + port);
+});
